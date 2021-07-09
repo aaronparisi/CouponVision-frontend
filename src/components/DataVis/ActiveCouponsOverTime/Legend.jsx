26fa2169e4cf4072f10f5192a14cf6ac9a5d0815
Aaron <parisi.aaron@gmail.com>
2021-07-09 19:19:55 +0000

    added select all, deselect all buttons to active over time

--- src/components/DataVis/ActiveCouponsOverTime/Legend.jsx
+++ src/components/DataVis/ActiveCouponsOverTime/Legend.jsx
@@ -14,55 +14,78 @@ const Legend = ({ selectedGrocers, setSelectedGrocers, hoverColor, setHoverColor
     )
   }
 
-  return (
-    <div className="legend">
-      {
-        Object.keys(selectedGrocers).map((grocerName, idx) => {
-          return (
-            <div 
-              className="grocer-checkbox" 
-              key={idx}
-              data-grocer-name={grocerName}
-              style={{ 
-                backgroundColor: (hoverColor === colors[grocerName]) ? hoverColor + "33" : "#eeeeee"
-              }}
-              onClick={e => {
-                e.preventDefault()
-                const checked = selectedGrocers[grocerName]
-                const color = colors[grocerName]
+  const overRide = (truthVal) => {
+    return Object
+      .fromEntries(Object.keys(selectedGrocers).map(grocer => [grocer, truthVal]))
+  }
 
-                if (!checked) {
-                  setHoverColor(color)
-                } else {
+  return (
+    <div className="legend-wrapper">
+      <div className="legend-buttons">
+        <button
+          onClick={e => {
+            setSelectedGrocers(overRide(true))
+          }}
+        >
+          Select All
+        </button>
+        <button
+          onClick={e => {
+            setSelectedGrocers(overRide(false))
+          }}
+        >
+          De-Select All
+        </button>
+      </div>
+      <div className="legend">
+        {
+          Object.keys(selectedGrocers).map((grocerName, idx) => {
+            return (
+              <div 
+                className="grocer-checkbox" 
+                key={idx}
+                data-grocer-name={grocerName}
+                style={{ 
+                  backgroundColor: (hoverColor === colors[grocerName]) ? hoverColor + "33" : "#eeeeee"
+                }}
+                onClick={e => {
+                  e.preventDefault()
+                  const checked = selectedGrocers[grocerName]
+                  const color = colors[grocerName]
+
+                  if (!checked) {
+                    setHoverColor(color)
+                  } else {
+                    setHoverColor(null)
+                  }
+            
+                  setSelectedGrocers({
+                    ...selectedGrocers,
+                    [grocerName]: !checked
+                  })
+                }}
+                onMouseEnter={e => {
+                  if (selectedGrocers[grocerName]) {
+                    setHoverColor(colors[grocerName])
+                  }
+                }}
+                onMouseLeave={e => {
                   setHoverColor(null)
-                }
-          
-                setSelectedGrocers({
-                  ...selectedGrocers,
-                  [grocerName]: !checked
-                })
-              }}
-              onMouseEnter={e => {
-                if (selectedGrocers[grocerName]) {
-                  setHoverColor(colors[grocerName])
-                }
-              }}
-              onMouseLeave={e => {
-                setHoverColor(null)
-              }}
-            >
-              <MyCheckbox 
-                grocerName={grocerName} 
-                color={colors[grocerName]}
-                checked={selectedGrocers[grocerName]}
-              />
-              <label htmlFor={`${grocerName}`}>
-                {grocerName}
-              </label>
-            </div>
-          )
-        })
-      }
+                }}
+              >
+                <MyCheckbox 
+                  grocerName={grocerName} 
+                  color={colors[grocerName]}
+                  checked={selectedGrocers[grocerName]}
+                />
+                <label htmlFor={`${grocerName}`}>
+                  {grocerName}
+                </label>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
